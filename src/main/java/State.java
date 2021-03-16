2e3b862767bea66fb565c94c1b5440b46d6e244c
Willem Ploegstra <willem.ploegstra@gmail.com>
2021-03-16 13:01:53 +0100

    added planet and started with state

---
+++ src/main/java/State.java
@@ -0,0 +1,33 @@
+import titan.StateInterface;
+
+public class State implements StateInterface
+{
+    Planet[] objects;
+
+
+    public State(Vector3d p0 , Vector3d v0)
+    {
+        objects = new Planet[11];//array containing all the objects in the system
+        objects[0]
+        objects[0]
+        objects[0]
+        objects[0]
+        objects[0]
+        objects[0]
+        objects[0]
+        objects[0]
+        objects[0]
+        objects[0]
+        objects[0]
+    }
+
+    public StateInterface addMul(double step, RateInterface rate)
+    {
+
+    }
+
+    public String toString()
+    {
+
+    }
+}
