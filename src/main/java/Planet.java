36a8fa2133e803ff66b51989ef621871caf7b9ee
Damian Postaru <72268415+damianpostaru@users.noreply.github.com>
2021-03-16 15:22:58 +0100

    Update Planet.java

--- src/main/java/Planet.java
+++ src/main/java/Planet.java
@@ -1,37 +1,48 @@
-public class Planet {
-    private final Vector3d pos;
-    private final Vector3d vel;
-    private final String name;
-    private final double mass;
+public class Planet
+{
+    private Vector3d pos;
+    private Vector3d vel;
+    private String name;
+    private double mass;
+    private double radius;
 
-    public Planet(Vector3d p0, Vector3d v0, double m, String n) {
+    public Planet(double m, double r,Vector3d p0,Vector3d v0, String n)
+    {
         pos = p0;
         vel = v0;
         name = n;
         mass = m;
+        radius = r;
     }
 
-    public void updatePos(Vector3d dp) {
+    public void updatePos(Vector3dInterface dp)
+    {
         pos.add(dp);
     }
-
-    public void updateVel(Vector3d dv) {
+    public void updateVel(Vector3dInterface dv)
+    {
         vel.add(dv);
     }
 
-    public double getMass() {
+    public double getMass()
+    {
         return mass;
     }
 
-    public Vector3dInterface getPos() {
+    public double getRadius() { return radius; }
+
+    public Vector3dInterface getPos()
+    {
         return pos;
     }
 
-    public Vector3dInterface getVel() {
+    public Vector3dInterface getVel()
+    {
         return vel;
     }
 
-    public String toString() {
-        return "[" + name + " pos: " + pos.toString() + " vel: " + vel.toString() + "]";
+    public String toString()
+    {
+        return "[" + name + " pos: " + pos.toString() + " vel: " + vel.toString()+ "]"; 
     }
 }
