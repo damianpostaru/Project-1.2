ceb88b0da7042f57f73b527018e79ca7b6d42bb3
Damian <postaru.damian@gmail.com>
2021-03-16 16:11:21 +0200

    Add second solver solve method. Add javafx to gradle plugin

--- src/main/java/Planet.java
+++ src/main/java/Planet.java
@@ -1,46 +1,37 @@
-import titan.Vector3dInterface;
+public class Planet {
+    private final Vector3d pos;
+    private final Vector3d vel;
+    private final String name;
+    private final double mass;
 
-public class Planet 
-{
-    private Vector3d pos;
-    private Vector3d vel;
-    private String name;
-    private double mass;
-
-    public Planet(Vector3d p0,Vector3d v0, double m, String n)
-    {
+    public Planet(Vector3d p0, Vector3d v0, double m, String n) {
         pos = p0;
         vel = v0;
         name = n;
         mass = m;
     }
 
-    public void updatePos(Vector3d dp)
-    {
+    public void updatePos(Vector3d dp) {
         pos.add(dp);
     }
-    public void updateVel(Vector3d dv)
-    {
+
+    public void updateVel(Vector3d dv) {
         vel.add(dv);
     }
 
-    public double getMass()
-    {
+    public double getMass() {
         return mass;
     }
 
-    public Vector3dInterface getPos()
-    {
+    public Vector3dInterface getPos() {
         return pos;
     }
 
-    public Vector3dInterface getVel()
-    {
+    public Vector3dInterface getVel() {
         return vel;
     }
 
-    public String toString()
-    {
-        return "[" + name + " pos: " + pos.toString() + " vel: " + vel.toString()+ "]"; 
+    public String toString() {
+        return "[" + name + " pos: " + pos.toString() + " vel: " + vel.toString() + "]";
     }
 }
--- src/main/java/Solver.java
+++ src/main/java/Solver.java
@@ -1,23 +1,36 @@
-public class Solver implements ODESolverInterface{
-	@Override
-	public StateInterface[] solve(ODEFunctionInterface f, StateInterface y0, double[] ts) {
-		StateInterface[] states = new StateInterface[ts.length];
-		states[0] = y0;
-		for (int i = 1; i < states.length; i++) {
+public class Solver implements ODESolverInterface {
 
-			// leave the step size as 0.1 for now
+    public Solver() {
+    }
 
-			// TODO Auto-generated method stub
-			// test to improve the step size (manual 2.1 Testing(p.4))
-			states[i] = step(f, ts[i] - ts[i - 1], states[i - 1], 0.1);
-		}
+    @Override
+    public StateInterface[] solve(ODEFunctionInterface f, StateInterface y0, double[] ts) {
+        StateInterface[] states = new StateInterface[ts.length];
+        states[0] = y0;
+        for (int i = 1; i < states.length; i++) {
 
-		return states;
-	}
+            // leave the step size as 0.1 for now
+
+            // test to improve the step size (manual 2.1 Testing(p.4))
+            states[i] = step(f, ts[i], states[i - 1], 0.1);
+        }
+
+        return states;
+    }
 
     @Override
     public StateInterface[] solve(ODEFunctionInterface f, StateInterface y0, double tf, double h) {
-        return new StateInterface[0];
+        StateInterface[] states = new StateInterface[(int) (tf / h) + 1];
+        states[0] = y0;
+        double t = 0;
+        int i = 1;
+        while (t <= tf) {
+            states[i] = step(f, t, states[i - 1], 0.1);
+            i++;
+            t += h;
+        }
+
+        return states;
     }
 
     @Override
