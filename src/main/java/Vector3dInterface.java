37c9efb034d13e0f8a00b601c792da7e3dbe7e0b
Isa yj <71944396+isayj@users.noreply.github.com>
2021-03-16 16:54:49 +0800

    interfaces updates from the examiners

--- src/main/java/Vector3dInterface.java
+++ src/main/java/Vector3dInterface.java
@@ -1,29 +1,24 @@
-/**
+/*
  * @author Pieter Collins, Christof Seiler, Katerina Stankova, Nico Roos, Katharina Schueller
  * @version 0.99.0
- * <p>
+ *
  * This interface serves as the API for students in phase 1.
  */
 
+package titan;
+
 public interface Vector3dInterface {
 
-    double getX();
+    public double getX();
+    public void setX(double x);
+    public double getY();
+    public void setY(double y);
+    public double getZ();
+    public void setZ(double z);
 
-    void setX(double x);
-
-    double getY();
-
-    void setY(double y);
-
-    double getZ();
-
-    void setZ(double z);
-
-    Vector3dInterface add(Vector3dInterface other);
-
-    Vector3dInterface sub(Vector3dInterface other);
-
-    Vector3dInterface mul(double scalar);
+    public Vector3dInterface add(Vector3dInterface other);
+    public Vector3dInterface sub(Vector3dInterface other);
+    public Vector3dInterface mul(double scalar);
 
     /**
      * Scalar x vector multiplication, followed by an addition
@@ -32,31 +27,31 @@ public interface Vector3dInterface {
      * @param other  the vector used in the multiplication step
      * @return the result of the multiplication step added to this vector,
      * for example:
-     * <p>
-     * Vector3d a = Vector();
-     * double h = 2;
-     * Vector3d b = Vector();
-     * ahb = a.addMul(h, b);
-     * <p>
+     *
+     *       Vector3d a = Vector();
+     *       double h = 2;
+     *       Vector3d b = Vector();
+     *       ahb = a.addMul(h, b);
+     *
      * ahb should now contain the result of this mathematical operation:
-     * a+h*b
+     *       a+h*b
      */
-    Vector3dInterface addMul(double scalar, Vector3dInterface other);
+    public Vector3dInterface addMul(double scalar, Vector3dInterface other);
 
     /**
      * @return the Euclidean norm of a vector
      */
-    double norm();
+    public double norm();
 
     /**
      * @return the Euclidean distance between two vectors
      */
-    double dist(Vector3dInterface other);
+    public double dist(Vector3dInterface other);
 
     /**
      * @return A string in this format:
      * Vector3d(-1.0, 2, -3.0) should print out (-1.0,2.0,-3.0)
      */
-    String toString();
+    public String toString();
 
 }
