55c00cb3c2a970c17917d9d51943cdff5d58d9c1
Damian <postaru.damian@gmail.com>
2021-03-17 18:35:32 +0200

    Implement second trajectory method

--- src/main/java/ProbeSimulator.java
+++ src/main/java/ProbeSimulator.java
@@ -1,8 +1,17 @@
 public class ProbeSimulator implements ProbeSimulatorInterface {
     @Override
     public Vector3dInterface[] trajectory(Vector3dInterface initialPosition, Vector3dInterface initialVelocity, double[] outputTimes) {
-
-        return new Vector3dInterface[0];
+        Vector3dInterface[] trajectory = new Vector3d[outputTimes.length];
+        State initialState = new State(initialPosition, initialVelocity);
+        Solver solver = new Solver();
+        State[] states = (State[]) solver.solve(new Function(), initialState, outputTimes);
+        for (int i = 0; i < states.length; i++) {
+            trajectory[i] = states[i].getSolarSystem().getShuttle().getPosition();
+            //System.out.println(states[i].getSolarSystem().getShuttle().getVel());
+        }
+//        System.out.println(states[states.length-1].getSolarSystem().get(8).getPos().dist(states[states.length-1].getSolarSystem().getShuttle().getPos()));
+//        System.out.println(states[states.length-1].getSolarSystem().get(8));
+        return trajectory;
     }
 
     @Override
