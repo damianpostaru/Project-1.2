3e7eff3e5a5dcfcc59630916331bc73e3a0ee38d
Willem Ploegstra <willem.ploegstra@gmail.com>
2021-03-18 11:47:42 +0100

    Added log lines to ProbeSimulator

--- src/main/java/ProbeSimulator.java
+++ src/main/java/ProbeSimulator.java
@@ -9,12 +9,30 @@ public class ProbeSimulator implements ProbeSimulatorInterface {
     public Vector3dInterface[] trajectory(Vector3dInterface initialPosition, Vector3dInterface initialVelocity, double finalTime, double stepSize) {
         Vector3dInterface[] trajectory = new Vector3d[(int) Math.ceil(finalTime / stepSize)];
         State initialState = new State(initialPosition, initialVelocity);
+        System.out.println("Probe starting position: " + initialPosition);
+        System.out.println("Probe starting velocity: " + initialVelocity);
+        System.out.println("Probe trajectory: ");
         Solver solver = new Solver();
         State[] states = (State[]) solver.solve(new Function(), initialState, finalTime, stepSize);
+        double bestDist = Double.MAX_VALUE;
+        double bestTime = 0;
         for (int i = 0; i < states.length; i++) {
             trajectory[i] = states[i].getSolarSystem().getShuttle().getPosition();
-            //System.out.println(states[i].getSolarSystem().getShuttle().getVel());
+            System.out.println(states[i].getSolarSystem().getShuttle().getPosition());
+            double dist = states[i].getSolarSystem().get(8).getPosition().dist(states[i].getSolarSystem().getShuttle().getPosition());
+            if(dist < bestDist)
+            {
+                bestDist = dist;
+                bestTime = i * stepSize;
+            }
         }
+        System.out.println("Time of closest approach: " + bestTime);
+        System.out.println("Distance of closest approach: " + bestDist);
+        if(bestDist <= states[states.length-1].getSolarSystem().get(8).getRadius())
+        {
+            System.out.println("Titan has been hit");
+        }
+
 //        System.out.println(states[states.length-1].getSolarSystem().get(8).getPos().dist(states[states.length-1].getSolarSystem().getShuttle().getPos()));
 //        System.out.println(states[states.length-1].getSolarSystem().get(8));
         return trajectory;
