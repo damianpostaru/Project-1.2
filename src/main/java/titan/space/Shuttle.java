d318a4a42f1766f80cf733bc2bf62d367c3630dc
Willem Ploegstra <willem.ploegstra@gmail.com>
2021-05-06 11:17:05 +0200

    changed engine specs to what the examiners specified

--- src/main/java/titan/space/Shuttle.java
+++ src/main/java/titan/space/Shuttle.java
@@ -8,11 +8,11 @@ public class Shuttle extends Planet {
     //NOTE: make sure that the burns do not overlap in their times
     //NOTE: for the most accurate result make sure the timings are divisible by the timestep used
     //NOTE: make sure engineDirections and engineTimings arrays are the same length
-    private static double[][] engineTimings = new double[][]{{80e6,0,900}};
+    private static double[][] engineTimings = new double[][]{{30e6,0,780}};
     private static Vector3d[] engineDirections = new Vector3d[]{new Vector3d(18044.44, -29351.0, -819.35)};
 
     private final double fuelMass;
-    private final double EFF_EXH_VEL = 40000;
+    private final double EFF_EXH_VEL = 20000;
 
     public Shuttle(Vector3d initialPosition, Vector3d initialVelocity, double fuelMass) {
         super(initialPosition, initialVelocity);
@@ -26,11 +26,10 @@ public class Shuttle extends Planet {
             if(engineTimings[i][2] >= t && engineTimings[i][1] < t) {//check if the engine needs to be fired and return the acceleration
                 System.out.println("Fire the engines!!!");
                 double force = engineTimings[i][0];
-                Vector3d dir = (Vector3d) engineDirections[i].mul(1.0/engineDirections[i].norm());//make sure the direction is a unit vector
+                Vector3d dir = (Vector3d) engineDirections[i].mul(1.0 / engineDirections[i].norm());//make sure the direction is a unit vector
                 double mass = calcFuelMass(t) + SolarSystemData.masses[11];
                 System.out.println(mass);
                 return (Vector3d) (dir.mul(force)).mul(1.0 / mass);
-
             }
         }
         return new Vector3d(0,0,0); //if no engine is used return no acceleration
