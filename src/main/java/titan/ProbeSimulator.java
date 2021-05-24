d0fb75333ebbcaa3485f484ffcba762b04ed484e
Willem Ploegstra <willem.ploegstra@gmail.com>
2021-05-24 11:35:47 +0200

    removed debugging statements from the code

--- src/main/java/titan/ProbeSimulator.java
+++ src/main/java/titan/ProbeSimulator.java
@@ -25,14 +25,12 @@ public class ProbeSimulator implements ProbeSimulatorInterface {
         State initialState = new State(initialPosition, initialVelocity);
         System.out.println("Probe starting position: " + initialPosition);
         System.out.println("Probe starting velocity: " + initialVelocity);
-//        System.out.println("Probe trajectory: ");
         Solver solver = new Solver();
         State[] states = (State[]) solver.solve(new Function(), initialState, finalTime, stepSize);
         double bestDist = Double.MAX_VALUE;
         double bestTime = 0;
         int bestIndex = -1;
-        Vector3d bestDiff = null;
-        int planetID = 3;
+        int planetID = 8;
         for (int i = 0; i < states.length; i++) {
             trajectory[i] = states[i].getSolarSystem().getShuttle().getPosition();
 
@@ -42,19 +40,10 @@ public class ProbeSimulator implements ProbeSimulatorInterface {
                 bestIndex = i;
                 bestDist = dist;
                 bestTime = i * stepSize;
-                bestDiff = (Vector3d) states[i].getSolarSystem().get(planetID).getPosition().sub(states[i].getSolarSystem().getShuttle().getPosition());
-            }
-            if(i > 590425 && i < 590450) {
-                //System.out.println((Vector3d) states[i].getSolarSystem().getShuttle().getPosition());
-                //System.out.println((Vector3d) states[i].getSolarSystem().get(planetID).getPosition());
-                System.out.println((Vector3d) states[i].getSolarSystem().getShuttle().getPosition().sub(states[i].getSolarSystem().get(planetID).getPosition()));
             }
         }
-        Vector3d extraDiff = (Vector3d) states[bestIndex-1].getSolarSystem().get(planetID).getPosition().sub(states[bestIndex-1].getSolarSystem().getShuttle().getPosition());
-        System.out.println("relative velocity: " + (Vector3d) bestDiff.sub(extraDiff));
         System.out.println("Time of closest approach: " + bestTime);
         System.out.println("Distance of closest approach: " + bestDist);
-        System.out.println("Diff of closest approach: " + bestDiff);
         System.out.println("Shuttle position of closest approach: " + states[bestIndex].getSolarSystem().get(planetID).getPosition());
         System.out.println("Body position of closest approach: " + states[bestIndex].getSolarSystem().getShuttle().getPosition());
 
