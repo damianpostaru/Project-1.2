0acf87c7b96fcf57d6ee127680d6bc848976501e
Willem Ploegstra <willem.ploegstra@gmail.com>
2021-06-14 16:16:14 +0200

    Started on the GUI for the lander

--- src/main/java/titan_lander/gui/LanderSimScene.java
+++ src/main/java/titan_lander/gui/LanderSimScene.java
@@ -1,11 +1,17 @@
 package titan_lander.gui;
 
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.LineTo;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
 import titan.gui.PlanetTransition;
+import titan.space.Vector3d;
 
 import static titan.gui.Util.startTimerTask;
 
@@ -17,7 +23,7 @@ public class LanderSimScene extends LanderVisualizer
         exitButton = new Button("Exit Simulation!");
 
         probeLaunch.setOnAction(e -> {
-            //animate lander
+            LanderSimScene.animateLander();
         });
 
         exitButton.setOnAction(e -> System.exit(0));
@@ -25,12 +31,34 @@ public class LanderSimScene extends LanderVisualizer
         buttonBox = new VBox(25);
         buttonBox.getChildren().addAll(probeLaunch,exitButton);
 
+        drawLanderPath();
+
         BorderPane root = new BorderPane();
+        root.setCenter(landerPath);
         root.setRight(buttonBox);
         root.getStyleClass().add("landerBackground");;
 
         visualiserScene = new Scene(root, screenBounds.getWidth(), screenBounds.getHeight());
         visualiserScene.getStylesheets().add("Stylesheet.css");
 
+    }
+
+    public static void drawLanderPath()
+    {
+        Vector3d firstVector = metersToPixels(landerPathVectors[0]);
+        MoveTo firstMove = new MoveTo(firstVector.getX(),firstVector.getY());
+        landerPath = new Path(firstMove);
+
+        for (int i = 1; i < landerPathVectors.length; i++)
+        {
+            Vector3d end = metersToPixels(landerPathVectors[i]);
+            LineTo line = new LineTo(end.getX(),end.getY());
+            landerPath.getElements().add(line);
+        }
+    }
+
+    public static void animateLander()
+    {
+        //animate lander
     }
 }
--- src/main/java/titan_lander/gui/LanderVisualizer.java
+++ src/main/java/titan_lander/gui/LanderVisualizer.java
@@ -2,21 +2,22 @@ package titan_lander.gui;
 
 import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.LineTo;
+import javafx.scene.shape.Path;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import titan.space.Vector3d;
-
-import java.awt.*;
 
 //Class to visualize a path, will create a window and show the lander at each position including the previous path
 
 public class LanderVisualizer extends Application
 {
-    protected  static Vector3d[] landerPath;
+    protected  static Vector3d[] landerPathVectors;
 
     protected static Stage singleStage;
     static Rectangle2D screenBounds;
@@ -24,38 +25,50 @@ public class LanderVisualizer extends Application
     protected static Scene introScene, visualiserScene;
     protected static Button beginButton, probeLaunch, exitButton;
     public static VBox buttonBox;
+    static final double CONVERSION_FACTOR = 3.2000e-03;
+    public static Group pathLines;
+    public static Path landerPath;
     @Override
     public void start(Stage primaryStage)
     {
-        centerX = 960;//position of the landing pad
-        centerY = 960;
+        //get the path of the lander from the simulation:
+
+        //temporary solution until we made the simulation work
+        landerPathVectors = new Vector3d[1000];
+        double height = 100000;
+
+        for (int i = 0; i < landerPathVectors.length; i++)
+        {
+            landerPathVectors[i] = new Vector3d(0,height - (i * (height/ landerPathVectors.length)),0);
+        }
+        landerPathVectors[0] = new Vector3d(0,100000,0);
+        landerPathVectors[1] = new Vector3d(0,0,0);
+
 
         singleStage = primaryStage;
         singleStage.setTitle("Titan Landing!");
         screenBounds = Screen.getPrimary().getBounds();
 
-        centerX = screenBounds.getWidth() / 2;
-        centerY = screenBounds.getHeight() / 2;
+        //position of the landing pad
+        centerX = 100;
+        centerY = screenBounds.getHeight();
 
         LanderIntroScene.setIntroScene();
         LanderSimScene.setVisualizerScene();
 
-        //get the path of the lander from the simulation:
 
-        //temporary solution until we made the simulation work
-        landerPath = new Vector3d[1000];
-        double height = 10000;
-
-        for (int i = 0; i < landerPath.length; i++)
-        {
-            landerPath[i] = new Vector3d(0,height - (i * (height/landerPath.length)),0);
-        }
 
         singleStage.setFullScreen(true);
         singleStage.setResizable(false);
         singleStage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
         singleStage.setScene(introScene);
         singleStage.show();
+    }
+
+    protected static Vector3d metersToPixels(Vector3d pos)
+    {
+        Vector3d newVector = new Vector3d(centerX + pos.getX() * CONVERSION_FACTOR ,centerY - pos.getY() * CONVERSION_FACTOR,0);
+        return newVector;
     }
 
     public static void main(String[] args)
