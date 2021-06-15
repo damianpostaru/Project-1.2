000d224622603eb21ca8a30f903201aa00ffd191
Damian Postaru <postaru.damian@gmail.com>
2021-06-15 17:45:43 +0300

    Remove all spring related stuff

--- src/main/java/io/group8/titan/server/repository/SolarSystemRepository.java
+++
@@ -1,9 +0,0 @@
-package io.group8.titan.server.repository;
-
-import io.group8.titan.space.SolarSystem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SolarSystemRepository extends JpaRepository<SolarSystem, Long> {
-}
