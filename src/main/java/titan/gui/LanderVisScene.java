fc0da53e1bbceb79e85f782c51228d03a665663a
Diogo Rio <72268280+diogorrio@users.noreply.github.com>
2021-06-19 16:14:18 +0100

    Add files via upload

---
+++ src/main/java/titan/gui/LanderVisScene.java
@@ -0,0 +1,79 @@
+package titan.gui;
+
+import javafx.animation.*;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.*;
+import javafx.util.Duration;
+import titan.gui.GuiMain;
+import titan.gui.PlanetTransition;
+import titan.space.Vector3d;
+
+import static titan.gui.Util.startTimerTask;
+
+public class LanderVisScene extends GuiMain
+{
+    public static void setLanderVisualizerScene()
+    {
+        landerProbeLaunch = new Button("Launch Lander!");
+        landerExitButton = new Button("Exit Simulation!");
+
+        landerProbeLaunch.setOnAction(e -> {
+            animateLander();
+        });
+
+        landerExitButton.setOnAction(e -> singleStage.setScene(visualiserScene));
+
+        landerButtonBox = new VBox(25);
+        landerButtonBox.getChildren().addAll(landerProbeLaunch,landerExitButton);
+
+        drawLanderPath();
+        pathLines = new Pane(landerPath);
+
+        BorderPane root = new BorderPane();
+        root.setCenter(pathLines);
+        root.setRight(landerButtonBox);
+        root.getStyleClass().add("landerBackground");;
+
+        landerVisualiserScene = new Scene(root, screenBounds.getWidth(), screenBounds.getHeight());
+        landerVisualiserScene.getStylesheets().add("Stylesheet.css");
+
+    }
+
+    public static void drawLanderPath()
+    {
+        MoveTo firstMove = new MoveTo(firstVector.getX(),firstVector.getY());
+        landerPath = new Path(firstMove);
+
+        for (int i = 1; i < landerPathVectors.length; i++)
+        {
+            Vector3d end = metersToPixels(landerPathVectors[i]);
+            LineTo line = new LineTo(end.getX(),end.getY());
+            landerPath.getElements().add(line);
+        }
+    }
+
+    public static void animateLander()
+    {
+        Timeline timeline = new Timeline();
+        timeline.setCycleCount(1);
+        timeline.setAutoReverse(false);
+
+        for (int i = 0; i < landerPathVectors.length; i++)
+        {
+            Vector3d v = metersToPixels(landerPathVectors[i]);
+
+            KeyValue rotation = new KeyValue(landerView.rotateProperty(),Math.toDegrees(v.getZ()));
+            KeyValue xPos = new KeyValue(landerView.xProperty(),v.getX() - imgSize/2);
+            KeyValue yPos = new KeyValue(landerView.yProperty(),v.getY() - imgSize/2);
+            timeline.getKeyFrames().add(new KeyFrame(Duration.millis(totalAnimTime/(landerFinalTime/landerTimeStep) * i),rotation,xPos,yPos));
+        }
+        timeline.play();
+    }
+}
+
