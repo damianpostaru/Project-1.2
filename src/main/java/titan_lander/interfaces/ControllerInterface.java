ec6fe6f1ec1f2c6fa2eeb0c0897624f2d36c4257
Diogo Rio <72268280+diogorrio@users.noreply.github.com>
2021-06-21 12:40:50 +0100

    Update ControllerInterface.java

--- src/main/java/titan_lander/interfaces/ControllerInterface.java
+++ src/main/java/titan_lander/interfaces/ControllerInterface.java
@@ -5,6 +5,8 @@ package titan_lander.interfaces;
  */
 public interface ControllerInterface {
 
+    public final double TITAN_G = 1.352;
+
     //methods to get the accelerations of the lander based on the current state and the time
     public double getX(AbstractLander lander, double t);
     public double getY(AbstractLander lander,double t);
